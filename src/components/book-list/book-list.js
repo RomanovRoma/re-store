51ac4538c804160a67cb4336bbf6121523de6315
RomanovRoma <romanian.reget@gmail.com>
2021-01-24 20:09:07 +0300

    refactor(BookList): refactor mapDispatchToProps

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -13,17 +13,7 @@ import './book-list.css'
 class BookList extends Component {
 
   componentDidMount() {
-    // 1. receive data
-    const {
-      bookstoreService,
-      booksLoaded,
-      booksRequested,
-      booksError } = this.props
-    booksRequested()
-    bookstoreService.getBooks()
-      // 2. dispatch action to store
-      .then((data) => booksLoaded(data))
-      .catch((err) => booksError(err))
+    this.props.fetchBookS()
   }
 
   render() {
@@ -54,10 +44,16 @@ const mapStateToProps = ({ books, loading, error }) => {
   return { books, loading, error }
 }
 
-const mapDispatchToProps = {
-  booksLoaded,
-  booksRequested,
-  booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps
+  return {
+    fetchBookS: () => {
+      dispatch(booksRequested());
+      bookstoreService.getBooks()
+        .then((data) => dispatch(booksLoaded(data)))
+        .catch((err) => dispatch(booksError(err)))
+    }
+  }
 }
 
 export default compose(
