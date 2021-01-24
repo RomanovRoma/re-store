3319f4098b5a578874ab9dfe694be7cdd79c80ef
RomanovRoma <romanian.reget@gmail.com>
2021-01-24 10:45:05 +0300

    feat: add reducer, action booksLoaded, store

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+
+cosnt initialState = {
+  books: []
+}
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case 'BOOKS_LOADED':
+      return {
+        books: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
