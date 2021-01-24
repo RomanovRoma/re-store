5f0276be190cd6f81492e8391ee59e5b716abd29
RomanovRoma <romanian.reget@gmail.com>
2021-01-24 18:43:39 +0300

    feat(BookstoreService): add Promise

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import BookListItem from '../book-list-item'
+import Spinner from '../spinner'
 import { connect } from 'react-redux'
 
 import { withBookstoreService } from '../hoc'
@@ -12,15 +13,18 @@ class BookList extends Component {
 
   componentDidMount() {
     // 1. receive data
-    const { bookstoreService } = this.props
-    const data = bookstoreService.getBooks()
-
-    // 2. dispatch action to store
-    this.props.booksLoaded(data)
+    const { bookstoreService, booksLoaded } = this.props
+    bookstoreService.getBooks()
+      // 2. dispatch action to store
+      .then((data) => booksLoaded(data));
   }
 
   render() {
-    const { books } = this.props
+    const { books, loading } = this.props
+
+    if (loading) {
+      return <Spinner />
+    }
     return (
       <ul className="book-list">
         {
@@ -35,8 +39,8 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({ books }) => {
-  return { books }
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading }
 }
 
 const mapDispatchToProps = {
