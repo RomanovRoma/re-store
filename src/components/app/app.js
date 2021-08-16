a043957189b281b8f99e76ea1f307b25b9c93263
RomanovRoma <romanian.reget@gmail.com>
2021-08-17 00:39:44 +0300

    deploy gh-pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { HomePage, CartPage } from '../pages'
 import ShopHeader from '../shop-header'
-import './app.css'
 
 const App = () => {
   return (
